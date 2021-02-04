ac4f747d3fa50ff0f51cccbe93f8e3699d064016
Marcin T <fake_mail@gmail.com>
2021-02-04 13:43:48 +0100

    move validation schema to a separate file

--- app.js
+++ app.js
@@ -5,7 +5,8 @@ const Campground = require('./models/campground');
 const methodOverride = require('method-override'); // to be able to send other types of requests than GET/POST from the form
 const catchAsync = require('./utils/catchAsync'); // function catching errors used instead of try/catch block 
 const ExpressError = require('./utils/ExpressError')
-const Joi = require('joi'); // a package used for validating data 
+//const Joi = require('joi'); // a package used for validating data (not required when we import the Schema from another file - here: schemas.js)
+const {campgroundSchema} = require('./schemas.js');
 const mongoose = require('mongoose');
 mongoose.connect('mongodb://localhost:27017/yelpcampDb', { useNewUrlParser: true, useCreateIndex: true, useUnifiedTopology: true })
     .then(() => {
@@ -15,7 +16,6 @@ mongoose.connect('mongodb://localhost:27017/yelpcampDb', { useNewUrlParser: true
         console.log('Database connection failed');
         console.log(err)
     })
-
 const path = require('path');
 
 app.set('view engine', 'ejs');
@@ -24,6 +24,18 @@ app.set('views', path.join(__dirname, 'views'));
 app.use(express.urlencoded({ extended: true }));
 app.use(methodOverride('_method'));
 app.engine('ejs', ejsMate);
+
+//middleware function used to validate campgrounds
+const validateCampground = (req,res,next) => {
+    const {error} = campgroundSchema.validate(req.body);
+    if(error){
+        const msg = error.details.map(el => el.message).join(',');
+        throw new ExpressError(msg, 400)
+    } else {
+        next();
+    }
+    //console.log(result);
+}
 
 app.get('/', (req, res) => {
     res.render('home');
@@ -38,23 +50,8 @@ app.get('/campgrounds/new', (req, res) => {
     res.render('campgrounds/new');
 })
 // adding a new campground (POST request)
-app.post('/campgrounds', catchAsync(async (req, res, next) => {
+app.post('/campgrounds', validateCampground, catchAsync(async (req, res, next) => {
         // if(!req.body.campground) throw new ExpressError ('Invalid Campgound Data'); // instead we gonna use 'Joi'
-        const campgroundSchema = Joi.object({
-            campground: Joi.object({
-                title: Joi.string().required(),
-                price: Joi.number().required().min(0),
-                image: Joi.string().required(),
-                location: Joi.string().required(),
-                description: Joi.string().required(),
-            }).required()
-        })
-        const {error} = campgroundSchema.validate(req.body);
-        if(error){
-            const msg = error.details.map(el => el.message).join(',');
-            throw new ExpressError(msg, 400)
-        }
-        console.log(result);
         const campground = new Campground(req.body.campground);
         await campground.save();
         res.redirect(`/campgrounds/${campground._id}`);
@@ -74,7 +71,7 @@ app.get('/campgrounds/:id/edit', catchAsync(async (req, res) => {
     res.render('campgrounds/edit', { campground })
 }))
 
-app.put('/campgrounds/:id', catchAsync(async (req, res, next) => {
+app.put('/campgrounds/:id', validateCampground, catchAsync(async (req, res, next) => {
     try {
         const { id } = req.params;
         const campground = await Campground.findByIdAndUpdate(id, { ...req.body.campground }); // (Ep. 411)
