41b3f26c0608ccff2c7cbc073ae85579e977c43b
Marcin T <fake_mail@gmail.com>
2021-03-10 19:05:22 +0100

    Restructuring - routes; add session/flash msgs

--- app.js
+++ app.js
@@ -1,13 +1,19 @@
 const express = require('express');
 const app = express();
 const ejsMate = require('ejs-mate');
+const session = require('express-session');
+const flash = require('connect-flash'); // pop-ups related to session data
 const Campground = require('./models/campground');
 const Review = require('./models/review');
 const methodOverride = require('method-override'); // to be able to send other types of requests than GET/POST from the form
-const catchAsync = require('./utils/catchAsync'); // function catching errors used instead of try/catch block 
-const ExpressError = require('./utils/ExpressError')
+//const catchAsync = require('./utils/catchAsync'); // function catching errors used instead of try/catch block 
+const ExpressError = require('./utils/ExpressError') // used for validating Reviews middleware
 //const Joi = require('joi'); // a package used for validating data (not required when we import the Schema from another file - here: schemas.js)
 const {campgroundSchema, reviewSchema} = require('./schemas.js');
+
+const campgrounds = require('./routes/campgrounds');
+const reviews = require('./routes/reviews');
+
 const mongoose = require('mongoose');
 mongoose.connect('mongodb://localhost:27017/yelpcampDb', { useNewUrlParser: true, useCreateIndex: true, useUnifiedTopology: true })
     .then(() => {
@@ -19,12 +25,35 @@ mongoose.connect('mongodb://localhost:27017/yelpcampDb', { useNewUrlParser: true
     })
 const path = require('path');
 
+app.engine('ejs', ejsMate);
 app.set('view engine', 'ejs');
 app.set('views', path.join(__dirname, 'views'));
 
 app.use(express.urlencoded({ extended: true }));
 app.use(methodOverride('_method'));
-app.engine('ejs', ejsMate);
+app.use(express.static(path.join(__dirname, 'public'))); // to tell express to serve PUBLIC Dierectory
+
+const sessionConfig = {
+    secret: 'thisshouldbeabettersecret!',
+    resave: false,
+    saveUninitialized: true,
+    cookie: {
+        httpOnly: true,
+        expires: Date.now() + 1000*60*60*24*7,
+        maxAge: 1000*60*60*24*7
+    }
+}
+
+app.use(session(sessionConfig));
+app.use(flash());
+
+// middleware to show flash messages; on every single request we gonna have access to whatever is in flash
+
+app.use((req,res,next) => {
+    res.locals.success = req.flash('success');
+    res.locals.error = req.flash('error');
+    next();
+})
 
 //middleware function used to validate campgrounds
 const validateCampground = (req,res,next) => {
@@ -48,72 +77,18 @@ const validateReview = (req,res,next) => {
     }
 }
 
+
+// to use routes from another file (all routes beginning with /campgrounds)
+app.use('/campgrounds', campgrounds);
+app.use('/campgrounds/:id/reviews', reviews);
+
 app.get('/', (req, res) => {
     res.render('home');
 })
-// listing all camps
-app.get('/campgrounds', async (req, res) => {
-    const campgrounds = await Campground.find({});
-    res.render('campgrounds/index', { campgrounds })
-})
-// adding a new campground(form route)
-app.get('/campgrounds/new', (req, res) => {
-    res.render('campgrounds/new');
-})
-// adding a new campground (POST request)
-app.post('/campgrounds', validateCampground, catchAsync(async (req, res, next) => {
-        // if(!req.body.campground) throw new ExpressError ('Invalid Campgound Data'); // instead we gonna use 'Joi'
-        const campground = new Campground(req.body.campground);
-        await campground.save();
-        res.redirect(`/campgrounds/${campground._id}`);
 
-}))
+// campground routes moved to 'routes' folder
 
-app.get('/campgrounds/:id', catchAsync(async (req, res) => {
-    //const {id} = req.params;
-    const campground = await Campground.findById(req.params.id).populate('reviews');
-    console.log(campground);
-    res.render('campgrounds/show', { campground })
-}))
-
-app.get('/campgrounds/:id/edit', catchAsync(async (req, res) => {
-    const { id } = req.params.id;
-    //const campground = await Campground.find({id});
-    const campground = await Campground.findById(req.params.id);
-    res.render('campgrounds/edit', { campground })
-}))
-
-app.put('/campgrounds/:id', validateCampground, catchAsync(async (req, res, next) => {
-    try {
-        const { id } = req.params;
-        const campground = await Campground.findByIdAndUpdate(id, { ...req.body.campground }); // (Ep. 411)
-        res.redirect(`/campgrounds/${campground._id}`);
-    } catch (e) {
-        next(e);
-    }
-}))
-
-app.delete('/campgrounds/:id', catchAsync(async (req, res) => {
-    const { id } = req.params;
-    await Campground.findByIdAndDelete(id)
-    res.redirect('/campgrounds/');
-}))
-
-app.post('/campgrounds/:id/reviews', validateReview, catchAsync(async(req,res) => {
-    const campground = await Campground.findById(req.params.id);
-    const review = new Review(req.body.review);
-    campground.reviews.push(review);
-    await review.save();
-    await campground.save();
-    res.redirect(`/campgrounds/${campground._id}`);
-}))
-
-app.delete('/campgrounds/:id/reviews/:reviewId', catchAsync(async(req,res) => {
-    const{id,reviewId} = req.params;
-    await Campground.findByIdAndUpdate(id, {$pull: {reviews: reviewId}});
-    await Review.findByIdAndDelete(reviewId);
-    res.redirect(`/campgrounds/${id}`);
-}))
+// review routes moved to 'routes' folder
 
 app.all('*', (req,res,next) => {
     //res.send('404!');
---
+++ routes/campgrounds.js
@@ -0,0 +1,80 @@
+const express = require('express');
+const router = express.Router(); // mergeParams not necessary cause we have the "ids" defined in the routes below (unlike review.js)
+const catchAsync = require('../utils/catchAsync'); // function catching errors used instead of try/catch block 
+const ExpressError = require('../utils/ExpressError');
+const Campground = require('../models/campground');
+const {campgroundSchema} = require('../schemas.js');
+
+//middleware function used to validate campgrounds
+const validateCampground = (req,res,next) => {
+    const {error} = campgroundSchema.validate(req.body);
+    if(error){
+        const msg = error.details.map(el => el.message).join(',');
+        throw new ExpressError(msg, 400)
+    } else {
+        next();
+    }
+    //console.log(result);
+}
+
+// we remove '/campgrounds' in the routes path (the beginning of the routes already defined in app.js - app.use())
+
+// listing all camps
+router.get('/', async (req, res) => {
+    const campgrounds = await Campground.find({});
+    res.render('campgrounds/index', { campgrounds })
+})
+// adding a new campground(form route)
+router.get('/new', (req, res) => {
+    res.render('campgrounds/new');
+})
+// adding a new campground (POST request)
+router.post('/', validateCampground, catchAsync(async (req, res, next) => {
+        // if(!req.body.campground) throw new ExpressError ('Invalid Campgound Data'); // instead we gonna use 'Joi'
+        const campground = new Campground(req.body.campground);
+        await campground.save();
+        req.flash('success', 'Successfully made a new campground!');
+        res.redirect(`/campgrounds/${campground._id}`);
+
+}))
+
+router.get('/:id', catchAsync(async (req, res) => {
+    //const {id} = req.params;
+    const campground = await Campground.findById(req.params.id).populate('reviews');
+    if(!campground){
+        req.flash('error', 'Cannot find that campground!');
+        res.redirect('/campgrounds');
+    }
+    res.render('campgrounds/show', { campground })
+}))
+
+router.get('/:id/edit', catchAsync(async (req, res) => {
+    const { id } = req.params.id;
+    //const campground = await Campground.find({id});
+    const campground = await Campground.findById(req.params.id);
+    if(!campground){
+        req.flash('error', 'Cannot find that campground!');
+        res.redirect('/campgrounds');
+    }
+    return res.render('campgrounds/edit', { campground })
+}))
+
+router.put('/:id', validateCampground, catchAsync(async (req, res, next) => {
+    try {
+        const { id } = req.params;
+        const campground = await Campground.findByIdAndUpdate(id, { ...req.body.campground }); // (Ep. 411)
+        req.flash('success', 'Successfully updated the campground!');
+        res.redirect(`/campgrounds/${campground._id}`);
+    } catch (e) {
+        next(e);
+    }
+}))
+
+router.delete('/:id', catchAsync(async (req, res) => {
+    const { id } = req.params;
+    await Campground.findByIdAndDelete(id);
+    req.flash('success', 'Campground successfully deleted!');
+    res.redirect('/campgrounds/');
+}))
+
+module.exports = router;
